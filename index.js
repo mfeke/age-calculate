3cc787e7e88985f5f4397ba0af49aa8e08facb9b
mfekethothulane <111047885+mfeke@users.noreply.github.com>
2023-05-05 19:37:03 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+let button = document.getElementById("btn")
+let res = document.getElementById("res")
+
+button.addEventListener("click", function ageCal(){
+    username = document.getElementById("username").value
+    bornYear= document.getElementById("bornYear").value
+    curYear = document.getElementById("currYear").value
+    sum = curYear-bornYear;
+    num = -1;
+
+    if(sum< num){
+        alert(`hi ${username} your are ${sum} year old`)
+       
+        
+    } else
+    {
+        
+    }
+
+     
+
+   
+})
